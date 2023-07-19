a892451c127c0b70d5832d79715b1199f2de4d17
Ian McIntyre <ianmcintyre12@hotmail.com>
2023-07-18 23:05:24 -0700

    changed private router to userdata based

--- ghi/src/utils/PrivateRoutes.js
+++ ghi/src/utils/PrivateRoutes.js
@@ -1,9 +1,17 @@
 import { Outlet, Navigate } from "react-router-dom";
 import useToken from "@galvanize-inc/jwtdown-for-react";
-
+import { UserContext } from "../components/UserContext";
+import { useContext } from "react";
 const PrivateRoutes = () => {
-  let auth = useToken();
-  return auth.token ? <Outlet /> : <Navigate to="/" />;
+    const {userData, setUserData} = useContext(UserContext
+    )
+      if (userData === null) {
+        return <Navigate to="/"/>
+      } else {
+        return < Outlet/>
+      }
+  // let auth = useToken();
+  // return auth.token ? <Outlet /> : <Navigate to="/" />;
 };
 
 export default PrivateRoutes;
