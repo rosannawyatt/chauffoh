d28441e5bb9ab4df381eee518edf8121bede4bf8
Rosanna Wyatt <rosannamollie@gmail.com>
2023-07-24 21:21:38 +0000

    Resolve "Unit test for list employees"

---
+++ api/tests/test_employees.py
@@ -0,0 +1,93 @@
+from fastapi.testclient import TestClient
+from main import app
+from queries.account_queries import AccountQueries
+
+client = TestClient(app)
+
+fake_employees_db = [
+    {
+        "id": 1,
+        "username": "bobb",
+        "hash_password": "89urnsduhn38u45giu",
+        "first_name": "Bob",
+        "last_name": "Belcher",
+        "email": "bob@email.com",
+        "is_employee": True,
+        "current_ride": False
+    },
+    {
+        "id": 2,
+        "username": "lindab",
+        "hash_password": "89urn76igh45giu",
+        "first_name": "Linda",
+        "last_name": "Belcher",
+        "email": "linda@email.com",
+        "is_employee": True,
+        "current_ride": False
+    }
+]
+
+
+class TestAccountQueries:
+    def update_account(self, _id, AccountUpdate):
+        result = {
+            "id": _id,
+            "username": "bobb",
+            "hash_password": "89urnsduhn38u45giu",
+            "first_name": "Bob",
+            "last_name": "Belcher",
+            "is_employee": True,
+            "current_ride": False
+        }
+        result.update(AccountUpdate)
+        return result
+
+
+class TestAllEmployees:
+    def get_all_employees(self):
+        return fake_employees_db
+
+
+def test_init():
+    assert 1 == 1
+
+
+def test_get_all_employees():
+    app.dependency_overrides[AccountQueries] = TestAllEmployees
+
+    response = client.get("api/employees")
+
+    app.dependency_overrides = {}
+
+    assert response.status_code == 200
+    assert response.json() == fake_employees_db
+
+
+def test_update_account():
+
+    app.dependency_overrides[AccountQueries] = TestAccountQueries
+
+    update = {
+        "username": "bobb",
+        "first_name": "Bob",
+        "last_name": "Belcher",
+        "email": "bob@burgers.com",
+        "is_employee": True,
+        "current_ride": False
+    }
+
+    response = client.patch("/api/accounts/3", json=update)
+
+    app.dependency_overrides = {}
+    print('test response', response.json())
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": 3,
+        "username": "bobb",
+        "hash_password": "89urnsduhn38u45giu",
+        "first_name": "Bob",
+        "last_name": "Belcher",
+        "email": "bob@burgers.com",
+        "is_employee": True,
+        "current_ride": False
+    }
